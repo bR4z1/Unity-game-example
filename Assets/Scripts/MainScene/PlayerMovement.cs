f1950621185a5ca6a6643f7428cf628d582fa100
braze31 <dthr31@gmail.com>
2018-12-06 11:46:17 +0300

    save & load

--- Assets/Scripts/MainScene/PlayerMovement.cs
+++ Assets/Scripts/MainScene/PlayerMovement.cs
@@ -25,6 +25,25 @@ public class PlayerMovement : MonoBehaviour {
         rb = player.GetComponent<Rigidbody2D>();
     }
 
+    // save. PlayerData, SaveSystem
+    // not saving health score etc.
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+
     private void OnTriggerStay2D(Collider2D col)
     {
         if (col.tag == "ladder")
